3a6bc4fdb4df2e385a1cc9e7e6d0ee7a3f7fa85a
Matthias Haeussler <maeddes@gmail.com>
2022-10-28 09:34:14 +0200

    added Spring Boot first app skeleton

---
+++ demo/src/main/java/com/example/demo/HftdemoApplication.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+// makes class eligible for incoming REST calls
+@RestController
+public class HftdemoApplication {
+
+	@RequestMapping("/")
+	public String sayHello(){
+
+		return "Good morning lovely people of HfT Stuttgart";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(HftdemoApplication.class, args);
+	}
+
+}
